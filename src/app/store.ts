c5cf2f93650bee2b7667e6d4e15305331cd2b718
meishimomura <mei.shimomura@icloud.com>
2021-03-29 08:42:59 +0900

    authSlice作成

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,11 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+
+import authReducer from "../features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
+    auth: authReducer,
     counter: counterReducer,
   },
 });
@@ -14,3 +17,5 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   unknown,
   Action<string>
 >;
+
+export type AppDispatch = typeof store.dispatch;
