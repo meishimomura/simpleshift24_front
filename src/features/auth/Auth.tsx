93d66fa8ac3aa928aee102548c62c21681e5d277
meishimomura <mei.shimomura@icloud.com>
2021-03-29 18:25:04 +0900

    認証画面マテリアルUI作成

--- src/features/auth/Auth.tsx
+++ src/features/auth/Auth.tsx
@@ -1,7 +1,18 @@
 import React, { useState } from "react";
 import styles from "./Auth.module.css";
 import { makeStyles, Theme } from "@material-ui/core/styles";
-import { TextField, Button } from "@material-ui/core";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 import { useSelector, useDispatch } from "react-redux";
 import { AppDispatch } from "../../app/store";
@@ -13,9 +24,23 @@ import {
   selectIsLoginView,
 } from "./authSlice";
 
-const useStyles = makeStyles((theme: Theme) => ({
-  button: {
-    margin: theme.spacing(3),
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(2),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(2),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 4),
   },
 }));
 
@@ -43,42 +68,68 @@ const Auth: React.FC = () => {
   };
   return (
     <div>
-      <h1>{isLoginView ? "ログイン" : "新規会員登録"}</h1>
-      <br />
-      <TextField
-        InputLabelProps={{
-          shrink: true,
-        }}
-        label="店舗番号"
-        type="text"
-        name="username"
-        value={credential.username}
-        onChange={handleInputChange}
-      />
-      <br />
-      <TextField
-        InputLabelProps={{
-          shrink: true,
-        }}
-        label="パスワード"
-        type="password"
-        name="password"
-        value={credential.password}
-        onChange={handleInputChange}
-      />
-
-      <Button
-        variant="contained"
-        color="primary"
-        size="small"
-        className={classes.button}
-        onClick={login}
-      >
-        {isLoginView ? "ログイン" : "新規会員登録"}
-      </Button>
-      <span onClick={() => dispatch(toggleMode())}>
-        {isLoginView ? "新規会員登録" : "ログイン"}
-      </span>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            {isLoginView ? "ログイン" : "新規会員登録"}
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              label="店舗番号"
+              type="text"
+              name="username"
+              value={credential.username}
+              autoFocus
+              onChange={handleInputChange}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              label="パスワード"
+              type="password"
+              name="password"
+              value={credential.password}
+              onChange={handleInputChange}
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              size="large"
+              color="secondary"
+              className={classes.submit}
+              onClick={login}
+            >
+              {isLoginView ? "ログイン" : "新規会員登録"}
+            </Button>
+            <Grid container justify="center" alignItems="center">
+              <Grid item>
+                <Link
+                  href="#"
+                  variant="body2"
+                  onClick={() => dispatch(toggleMode())}
+                >
+                  {isLoginView ? "新規会員登録" : "ログイン"}
+                </Link>
+              </Grid>
+            </Grid>
+            {/* <span onClick={() => dispatch(toggleMode())}>
+              {isLoginView ? "新規会員登録" : "ログイン"}
+            </span> */}
+          </form>
+        </div>
+      </Container>
     </div>
   );
 };
