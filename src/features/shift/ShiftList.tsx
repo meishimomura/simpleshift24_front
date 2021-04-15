82bfa8230c622d2d77278410ebb0ea97a57a0c44
meishimomura <mei.shimomura@icloud.com>
2021-04-15 17:45:52 +0900

    ShiftListコンポーネント新規シフト追加ボタン処理

--- src/features/shift/ShiftList.tsx
+++ src/features/shift/ShiftList.tsx
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import { Button } from "@material-ui/core";
+
+import { selectShifts, editShift, selectShift } from "./shiftSlice";
+import { AppDispatch } from "../../app/store";
+import { initialState } from "./shiftSlice";
+import { SHIFT_PAGE_STATE } from "../types";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  button: {
+    margin: theme.spacing(3),
+  },
+  small: {
+    margin: "auto",
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  },
+}));
 
 const ShiftList: React.FC = () => {
-  return <div></div>;
+  const classes = useStyles();
+  const dispatch: AppDispatch = useDispatch();
+  const shifts = useSelector(selectShifts);
+
+  const [state, setState] = useState<SHIFT_PAGE_STATE>({
+    rows: shifts,
+    offset: 0,
+    parPage: 10,
+  });
+
+  useEffect(() => {
+    setState((state) => ({
+      ...state,
+      rows: shifts,
+    }));
+  }, [shifts]);
+
+  return (
+    <>
+      <Button
+        className={classes.button}
+        variant="contained"
+        color="secondary"
+        size="small"
+        startIcon={<AddCircleOutlineIcon />}
+        onClick={() => {
+          dispatch(
+            editShift({
+              id: 0,
+              shift_date: "",
+              shift_start: "",
+              shift_end: "",
+              staff: 1,
+            })
+          );
+          dispatch(selectShift(initialState.selectedShift));
+        }}
+      >
+        新規シフト追加
+      </Button>
+    </>
+  );
 };
 
 export default ShiftList;
